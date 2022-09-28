623f97df15dca4da6f65b70ac6dd999a10bec87f
Vlad-UA-KR <vlad.fedorchenkog@gmail.com>
2022-09-28 18:21:33 +0300

    finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(function () {
+
 	$('.header__slider').slick({
 		slidesToShow: 1,
 		arrows: false,
@@ -9,6 +10,7 @@ $(function () {
 });
 
 $(function () {
+
 	$('.navigation').click(function (e) {
 
 		if ($(e.target.closest('div')).hasClass('navigation__burger')) {
@@ -17,12 +19,16 @@ $(function () {
 			return
 		};
 
-		$('.navigation__burger, .navigation__menu').removeClass('active');
-		$('body').removeClass('lock');
+		if ($(e.target.closest('a')).hasClass('navigation__link')) {
+			$('.navigation__burger, .navigation__menu').removeClass('active');
+			$('body').removeClass('lock');
+			return
+		};
 	})
 });
 
 $(function () {
+
 	$('.load').click(function (e) {
 		e.preventDefault();
 
@@ -30,4 +36,31 @@ $(function () {
 			$('.works__content-container').append('<img src="./img/works_image.png" alt="pika">');
 		}
 	})
+})
+
+$(function () {
+
+	$('.slider-page__slider-for').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		initialSlide: 2,
+		asNavFor: '.slider-page__slider-nav',
+		infinite: false
+	});
+
+	$('.slider-page__slider-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		initialSlide: 2,
+		variableWidth: true,
+		asNavFor: '.slider-page__slider-for',
+		centerMode: true,
+		focusOnSelect: true,
+		speed: 500,
+		prevArrow: '<i class="prevArr fa-solid fa-chevron-left"></i>',
+		nextArrow: '<i class="nextArr fa-solid fa-chevron-right"></i>',
+		infinite: false
+	});
 })
